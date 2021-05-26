385b6949d149b26173de646f63de11eeaf7c092e
Axel21204 <84819402+Axel21204@users.noreply.github.com>
2021-05-25 19:31:39 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDJzjX2OQODC7vFXfQ2op-WPVdrZ4FP96M",
+    authDomain: "axel24204aunt.firebaseapp.com",
+    projectId: "axel24204aunt",
+    storageBucket: "axel24204aunt.appspot.com",
+    messagingSenderId: "1074014888749",
+    appId: "1:1074014888749:web:13007ed774b3a186b4e9a4",
+    measurementId: "G-J6DX2E2JSG"
+    });
